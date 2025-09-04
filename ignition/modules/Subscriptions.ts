16d69144c0c77f949dcb799f1b5de2fa2aa25d05
ctrlprogrammer <sebastianto1999@gmail.com>
2025-09-03 20:44:26 -0500

    Deployments

--- ignition/modules/Subscriptions.ts
+++ ignition/modules/Subscriptions.ts
@@ -4,7 +4,7 @@ export default buildModule("SubscriptionsModule", (m) => {
   const subscriptions = m.contract("Subscriptions");
   m.call(subscriptions, "unpause");
   m.call(subscriptions, "setPayment", [m.contract("Token")]);
-  m.call(subscriptions, "createSubscription", ["Basic", 5000000000000000000n, 2592000n, "1"], { id: "call_1" });
-  m.call(subscriptions, "createSubscription", ["Pro", 10000000000000000000n, 2592000n, "2"], { id: "call_2" });
+  m.call(subscriptions, "createSubscription", ["1", 5000000000000000000n, 2592000n, "1"], { id: "call_1" });
+  m.call(subscriptions, "createSubscription", ["2", 10000000000000000000n, 2592000n, "2"], { id: "call_2" });
   return { subscriptions };
 });
